e9b2e43ebeacf8b13f5450c4813d7cc02f74e732
BR7braj <br7braj14@gmail.com>
2021-12-26 23:08:11 +0530

    Adding PUT, DELETE methods

--- src/main/java/com/brajesh/cacheingdemo/controller/ProductController.java
+++ src/main/java/com/brajesh/cacheingdemo/controller/ProductController.java
@@ -6,10 +6,14 @@ import com.brajesh.cacheingdemo.modal.Product;
 import com.brajesh.cacheingdemo.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class ProductController {
@@ -34,5 +38,23 @@ public class ProductController {
 
         return service.getProductById(Integer.parseInt(id));
     }
+
+    @PostMapping(path = "/product")
+    public Product saveProduct(@RequestBody Product product) {
+        
+        return service.saveProduct(product);
+    }
+
+    @PutMapping(path ="/product/modify/{id}")
+    public Product updateProduct(@RequestBody Product product , @PathVariable String id) throws Exception {
+        return service.updateProduct(product , Integer.parseInt(id));
+    }
+
+    @DeleteMapping(path = "/product/delete/{id}")
+    public String deleteProduct(@PathVariable String id) throws NumberFormatException, Exception {
+        service.deleteProduct(id);
+        return String.format("Deleted product with id : %s", id);
+    }
+    
     
 }
--- src/main/java/com/brajesh/cacheingdemo/service/ProductService.java
+++ src/main/java/com/brajesh/cacheingdemo/service/ProductService.java
@@ -7,6 +7,8 @@ import com.brajesh.cacheingdemo.modal.Product;
 import com.brajesh.cacheingdemo.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -26,9 +28,30 @@ public class ProductService {
         return repository.findAll();
     }
 
-    @Cacheable(value = "Product" , key = "#id")
+    @Cacheable(value = "Product"  , key = "#id")
     public Product getProductById(int id)
     {
         return Optional.ofNullable(repository.findById(id)).get().get();
     }
+
+    public Product saveProduct(Product product)
+    {
+        return repository.save(product);
+    }
+
+    @CachePut(value = "Product" , key = "#id")
+    public Product updateProduct(Product product, int id) throws Exception {
+        Product savedProduct = repository.findById(id).orElseThrow(() -> new Exception());
+        savedProduct.setId(product.getId());
+        savedProduct.setName(product.getName());
+        return repository.save(savedProduct);
+
+    }
+
+    @CacheEvict(value = "Product"  , key = "#id")
+    public void deleteProduct(String id) throws NumberFormatException, Exception {
+        Product product = repository.findById(Integer.parseInt(id)).orElseThrow(() ->  new Exception());
+        repository.delete(product);
+    }
+
 }
